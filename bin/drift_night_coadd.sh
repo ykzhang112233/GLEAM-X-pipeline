be09c4cda01523b777bb8cb29e5cb81b813d4af5
Tim Galvin <tim.galvin@curtin.edu.au>
2021-05-28 15:31:26 +0800

    night coadd / xy phase slope correction
    Squashed commit of the following:
    
    commit ab2c8d3f4658d1a6c118f1497ec8bdcbb614ce34
    Author: Tim Galvin <tim.galvin@curtin.edu.au>
    Date:   Thu May 27 14:58:39 2021 +0800
    
        nrgating phase slope
    
    commit ef4df0dad1024cd8d6e5a50f1f1be73efc4736e1
    Author: Tim Galvin <tim.galvin@curtin.edu.au>
    Date:   Mon May 24 16:00:38 2021 +0800
    
        phase slop corrections introduced
    
    commit ffbecc2766122510bbed5ac5664aba6ba4bb6e54
    Author: Tim Galvin <tim.galvin@curtin.edu.au>
    Date:   Tue May 18 16:41:42 2021 +0800
    
        using psf_combine_nights instead of swarp beam params
    
    commit 15d324f16a4dfb88d8d1a0ee3e30ffbb519fd61a
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Tue May 18 09:43:52 2021 +0800
    
        options and general tweaking
    
    commit 480777c042a859b1fb669917830b70cdedef18b0
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Mon May 17 18:05:23 2021 +0800
    
        rms weighting / write out / progress bar
    
    commit e7432b74952aa39d394e8437bdb38af48c49cad2
    Author: Tim Galvin <tim.galvin@curtin.edu.au>
    Date:   Mon May 17 12:33:59 2021 +0800
    
        coadding resampled images/beam works
    
    commit 1df36e8dea6dc4c7272cf2a926ca7ce03ae13895
    Author: Tim Galvin <tim.galvin@curtin.edu.au>
    Date:   Sun May 16 21:07:56 2021 +0800
    
        copy folder / resample directory
    
    commit b47a2d463d467830b863920c828d864f0fac0824
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 14 17:06:14 2021 +0800
    
        copy instead of symlink
    
    commit 7d584f6f57a9f7300f1d6da2ef84f2910d45bb0f
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 14 16:56:18 2021 +0800
    
        unique namer for existing night mosaics
    
    commit 6a8bfe97d2ee1131a183fb6535ea31ee71754d97
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 14 15:40:57 2021 +0800
    
        typo file expansion
    
    commit 9529af2342958e73f76b788213bd0565eec9e312
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 14 15:27:30 2021 +0800
    
        typo for path expansion
    
    commit 58faa1847664d3858aa3cfb3d7ac05e68ca99532
    Author: Tim Galvin <tim.galvin@curtin.edu.au>
    Date:   Sun May 9 12:17:22 2021 +0800
    
        typo fixes
    
    commit 3f223b871418c87c0b2d01502a9b24f0f453e77c
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 7 16:36:41 2021 +0800
    
        help message
    
    commit 182b0fadd6371a5b53de350d79fe524cd11931c4
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 7 16:34:56 2021 +0800
    
        form correct loh name
    
    commit 770990e9870824beb9886c3961aad66625f33cce
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 7 16:31:03 2021 +0800
    
        using correct sed marker
    
    commit fd9168c1810588945cdb3725455ea85b4fa6220e
    Author: Tim Galvin <tgalvin@curtin.edu.au>
    Date:   Fri May 7 15:22:49 2021 +0800
    
        combine night long mosaics

---
+++ bin/drift_night_coadd.sh
@@ -0,0 +1,140 @@
+#! /bin/bash
+
+usage()
+{
+echo "drift_night_coadd.sh [-p project] [-d dep] [-q queue] [-a account] [-t] [-r ra] [-e dec] -o list_of_nights.txt
+
+Task to combine mosaics that have been produced by 'drift_mosaic.sh' into a single, larger image. The contents of
+list_of_nights should be directed to the adopted mosaic folder for each nigh, incase a non-default 'mosaic' folder
+name was used.
+
+  -p project  : project, (must be specified, no default)
+  -d dep     : job number for dependency (afterok)
+  -t          : test. Don't submit job, just make the batch file
+                and then return the submission command
+  -r RA       : Right Ascension (decimal hours; default = guess from observation list)
+  -e dec      : Declination (decimal degrees; default = guess from observation list)
+  -m mosaicdir: Directory name for mosaics to be created (default = mosaics) 
+  nightlist  : the list of nights with existing coadded images to process" 1>&2;
+exit 1;
+}
+
+pipeuser=$(whoami)
+
+#initial variables
+dep=
+queue="-p ${GXSTANDARDQ}"
+tst=
+ra=
+dec=
+mosaicdir=
+
+# parse args and set options
+while getopts ':td:p:o:r:e:m:' OPTION
+do
+    case "$OPTION" in
+    d)
+        dep=${OPTARG} ;;
+    p)
+        project=${OPTARG} ;;
+	o)
+	    obslist=${OPTARG} ;;
+    r)
+        ra=${OPTARG} ;;
+    e)
+        dec=${OPTARG} ;;
+    m) 
+        mosaicdir=${OPTARG} ;;
+    t)
+        tst=1 ;;
+    ? | : | h)
+            usage ;;
+  esac
+done
+# set the nightlist to be the first non option
+shift  "$(($OPTIND -1))"
+nightlist=$1
+
+# if obslist is not specified or an empty file then just print help
+
+if [[ -z ${nightlist} ]] || [[ ! -s ${nightlist} ]] || [[ ! -e ${nightlist} ]] || [[ -z $project ]]
+then
+    usage
+fi
+
+if [[ ! -z ${dep} ]]
+then
+    depend="--dependency=afterok:${dep}"
+fi
+
+if [[ ! -z ${GXACCOUNT} ]]
+then
+    account="--account=${GXACCOUNT}"
+fi
+
+queue="-p ${GXSTANDARDQ}"
+base="${GXSCRATCH}/${project}"
+
+obss=($(sort "${nightlist}"))
+listbase=$(basename "${nightlist}")
+listbase=${listbase%%.*}
+script="${GXSCRIPT}/nightcoadd_${listbase}.sh"
+
+cat "${GXBASE}/templates/nightcoadd.tmpl" | sed -e "s:NIGHTLIST:${nightlist}:g" \
+                                      -e "s:RAPOINT:${ra}:g" \
+                                      -e "s:DECPOINT:${dec}:g" \
+                                      -e "s:BASEDIR:${base}:g" \
+                                      -e "s:MOSAICDIR:${mosaicdir}:g" \
+                                      -e "s:PIPEUSER:${pipeuser}:g" > "${script}"
+
+output="${GXLOG}/nightcoadd_${listbase}.o%A_%a"
+error="${GXLOG}/nightcoadd_${listbase}.e%A_%a"
+
+chmod 755 "${script}"
+
+# sbatch submissions need to start with a shebang
+echo '#!/bin/bash' > "${script}.sbatch"
+echo "singularity run ${GXCONTAINER} ${script}" >> "${script}.sbatch"
+
+# Automatically runs a job array for each sub-band
+sub="sbatch  --begin=now+5minutes --array=0-24  --export=ALL  --time=20:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --output=${output} --error=${error}"
+sub="${sub} ${GXNCPULINE} ${account} ${GXTASKLINE} ${depend} ${queue} ${script}.sbatch"
+if [[ ! -z ${tst} ]]
+then
+    echo "script is ${script}"
+    echo "submit via:"
+    echo "${sub}"
+    exit 0
+fi
+
+# submit job
+jobid=($(${sub}))
+jobid=${jobid[3]}
+
+# rename the err/output files as we now know the jobid
+error=${error//%A/"${jobid}"}
+output=${output//%A/"${jobid}"}
+
+freqs=(072-080MHz 072-103MHz 080-088MHz 088-095MHz 095-103MHz 103-111MHz 103-134MHz 111-118MHz
+118-126MHz 126-134MHz 139-147MHz 139-170MHz 147-154MHz 154-162MHz 162-170MHz 170-177MHz
+170-200MHz 177-185MHz 185-193MHz 193-200MHz 200-208MHz 200-231MHz 208-216MHz 216-223MHz
+223-231MHz)
+echo "Submitted ${script} as ${jobid} . Follow progress here:"
+
+# record submission
+for taskid in $(seq 0 1 24)
+do
+    terror="${error//%a/${taskid}}"
+    toutput="${output//%a/${taskid}}"
+    freq=${freqs[$taskid]}
+
+    echo "${toutput}"
+    echo "${terror}"
+
+    # if [ "${GXTRACK}" = "track" ] 
+    # then
+    #     ${GXCONTAINER} track_task.py queue_mosaic --jobid="${jobid}" --taskid="${taskid}" --task='mosaic' --submission_time="$(date +%s)" --batch_file="${script}" \
+    #                             --batch_obs_id "${obss[@]}" --stderr="${terror}" --stdout="${toutput}" \
+    #                             --subband="${freq}"
+    # fi
+done
