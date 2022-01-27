34040832803b85646d689da929c5ea8c62efdda4
tim <tim.galvin@curtin.edu.au>
2022-01-27 13:32:31 +0800

    task name corrected in subtted script

--- bin/obs_zip_folder.sh
+++ bin/obs_zip_folder.sh
@@ -93,7 +93,7 @@ else
 fi
 
 # start the real program
-script="${GXSCRIPT}/zip_ms_${obsnum}.sh"
+script="${GXSCRIPT}/zip_folder_${obsnum}.sh"
 cat "${GXBASE}/templates/zip_folder.tmpl" | sed -e "s:OBSNUM:${obsnum}:g" \
                                  -e "s:BASEDIR:${base}:g" \
                                  -e "s:ZIPMODE:${mode}:g" \
