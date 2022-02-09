0986edaa7b44dcc62bc5b4c12e563cc41891cb45
tim <tim.galvin@curtin.edu.au>
2022-02-09 11:34:10 +0800

    replaced explicit paths with glob

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from distutils.core import setup
 
 reqs = [
@@ -14,51 +15,7 @@ reqs = [
     "psutil",
 ]
 
-# TODO: See if glob works here
-scripts = [
-    "gleam_x/bin/pyhead.py",
-    "gleam_x/bin/alt_az_corrector.py",
-    "gleam_x/bin/aocal_diff.py",
-    "gleam_x/bin/aocal_phaseref.py",
-    "gleam_x/bin/aocal_ratio.py",
-    "gleam_x/bin/beam_value_at_radec.py",
-    "gleam_x/bin/calc_pointing.py",
-    "gleam_x/bin/calc_mean_pos.py",
-    "gleam_x/bin/clip_clean_components.py",
-    "gleam_x/bin/crop_catalogue.py",
-    "gleam_x/bin/dd_flux_mod.py",
-    "gleam_x/bin/extract_lowhigh_psf_beam.py",
-    "gleam_x/bin/filter_blurred_obsids.py",
-    "gleam_x/bin/generate_beam_list.py",
-    "gleam_x/bin/generate_weight_map.py",
-    "gleam_x/bin/iono_update.py",
-    "gleam_x/bin/make_imstack.py",
-    "gleam_x/bin/make_time_cube.py",
-    "gleam_x/bin/mask_image.py",
-    "gleam_x/bin/match_idg_obsid_pairs.py",
-    "gleam_x/bin/mosaic_global_rescale.py",
-    "gleam_x/bin/ms_flag_by_uvdist.py",
-    "gleam_x/bin/multiply.py",
-    "gleam_x/bin/new_fk5_template.py",
-    "gleam_x/bin/polyfit_snapshots.py",
-    "gleam_x/bin/psf_combine_axes.py",
-    "gleam_x/bin/psf_combine_nights.py",
-    "gleam_x/bin/psf_create.py",
-    "gleam_x/bin/psf_projected.py",
-    "gleam_x/bin/psf_select.py",
-    "gleam_x/bin/pyhead.py",
-    "gleam_x/bin/track_task.py",
-    "gleam_x/bin/vo2model.py",
-    "gleam_x/bin/fits_trim.py",
-    "gleam_x/bin/check_assign_solutions.py",
-    "gleam_x/bin/generate_ateam_subtract_model.py",
-    "gleam_x/db/check_sources_vs_obsids.py",
-    "gleam_x/db/check_src_fov.py",
-    "gleam_x/db/import_observations_from_db.py",
-    "gleam_x/utils/download_obsid_list.py",
-    "gleam_x/utils/flag_tiles_bad_dipoles.py",
-    "gleam_x/utils/obsid_ops.py",
-]
+scripts = glob('gleam_x/*/*py')
 
 setup(
     name="gleam_x",
