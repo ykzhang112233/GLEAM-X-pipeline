9314b21d19484af6a556367cb6bd1affbf1a2c07
astrokatross <kathryn.ross@icrar.org>
2022-10-28 16:19:00 +0800

    ✨ remove high RMS obsids, plot io

--- gleam_x/bin/filter_badionosphere.py
+++ gleam_x/bin/filter_badionosphere.py
@@ -23,7 +23,8 @@ import logging
 import matplotlib.ticker as ticker
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
-import cmasher as cmr
+# import cmasher as cmr
+import matplotlib
 
 
 logger = logging.getLogger(__name__)
@@ -71,6 +72,8 @@ def remove_missing(obsids_list):
     logger.debug(f"Number of missing obsids: {len(missing_obsids)}")
     if len(missing_obsids)>5:
         logger.warning(f"Large number of missing obsids: {len(missing_obsids)}/{len(obsids_list)}")
+    
+    good_obsids = np.asarray(good_obsids)
 
     return good_obsids, missing_obsids
 
@@ -110,8 +113,16 @@ def crossmatch_cats(
     ref_cat,
     sep=1,
 ):
+    ref_cat_file = f"{ref_cat}"
+    if os.path.exists(ref_cat_file):
+        hdu = fits.open(ref_cat_file)
+        refcat = hdu[1].data
+        hdu.close()
+    else:
+        logger.warning(f"Can't find reference GGSM catalogue for xm! ")
+        return input_cat
 
-    ref_cat_skycoords = SkyCoord(ref_cat.RAJ2000,ref_cat.DEJ2000, frame='fk5',unit=u.deg)
+    ref_cat_skycoords = SkyCoord(refcat.RAJ2000,refcat.DEJ2000, frame='fk5',unit=u.deg)
     input_cat_skycoords = SkyCoord(input_cat.ra, input_cat.dec, frame='fk5', unit=u.deg)
 
     idx, d2d, d3d = input_cat_skycoords.match_to_catalog_sky(ref_cat_skycoords)
@@ -121,10 +132,10 @@ def crossmatch_cats(
 
     return output_cat 
 
-def check_io(obsid):
+def check_io(obsid, do_xm):
 
     # for i in range(len(obsids_list)):
-    catfile = f"{args.project}/{obsid:10.0f}/{obsid:10.0f}_deep-MFS-image-pb_warp_rms.fits"
+    catfile = f"{args.project}/{obsid:10.0f}/{obsid:10.0f}_deep-MFS-image-pb_warp_rescaled_comp.fits"
     if os.path.exists(catfile):
         hdu = fits.open(catfile)
         temp_cat = hdu[1].data
@@ -136,7 +147,7 @@ def check_io(obsid):
     int_over_peak = temp_cat["int_flux"]/temp_cat["peak_flux"]
     err_intoverrms = temp_cat["err_int_flux"]/temp_cat["local_rms"]
     snr = temp_cat["int_flux"]/temp_cat["local_rms"]   
-    shape = temp_cat["a"]/temp_cat["b"]
+    shape = temp_cat["b"]/temp_cat["a"]
 
     mask = np.where((int_over_peak<=2)&(err_intoverrms<=2)&(snr>=5))
     cat = temp_cat[mask]
@@ -144,7 +155,14 @@ def check_io(obsid):
     if args.plot == "all":
         plt_io_obsid(int_over_peak[mask], shape[mask], f"{obsid:10.0f}")
 
-    cat_xm = crossmatch_cats(cat, args.refcat)
+
+    if do_xm == True:
+        cat_xm = crossmatch_cats(cat, args.refcat)
+
+    else:
+        return cat, [np.nanmean(int_over_peak[mask]), np.nanstd(int_over_peak[mask])], [np.nanmean(shape[mask]),np.nanstd(shape[mask])]
+    
+    
 
 
     return cat_xm, [np.nanmean(int_over_peak[mask]), np.nanstd(int_over_peak[mask])], [np.nanmean(shape[mask]),np.nanstd(shape[mask])]
@@ -162,15 +180,15 @@ def plt_io_obsid(
     fig = plt.figure(dpi=plt.rcParams['figure.dpi']*4.0)
     ax = fig.add_subplot(1,1,1)
 
-    ax.scatter(intoverpeak,shape,s=50, color="C6")
+    ax.scatter(intoverpeak,np.log(shape),s=50, color="C6")
     ax.axhline(1,color="k",alpha=0.3, linestyle="--")
 
     ax.set_xlabel("int_flux/peak_flux")
-    ax.set_ylabel("shape (a/b)")
+    ax.set_ylabel("log(shape) (b/a)")
     fig.suptitle(f"{obsid}: Int/peak vs shape")
 
     plt.savefig(f"{args.project}/{obsid}/{obsid}_intoverpeak_shape.{ext}", bbox_inches='tight')
-
+    plt.close(fig)
     return 
 
 def plt_io_pernight(
@@ -179,22 +197,28 @@ def plt_io_pernight(
     std_intoverpeak,
     shape,
     drift,
+    chans,
     ext="png",
+    cmap = plt.get_cmap("gnuplot2"),
 ):
-    colors=cmr.take_cmap_colors(
-        "cmr.flamingo", len(obslist), cmap_range=(0.4, 0.7), return_fmt="hex"
-    )
+
+    c_array = np.linspace(0,1,len(obslist)+4)
+    colors = cmap(c_array)
+    # colors=cmr.take_cmap_colors(
+    #     "cmr.flamingo", len(obslist), cmap_range=(0.4, 0.7), return_fmt="hex"
+    # )
     fig = plt.figure(dpi=plt.rcParams['figure.dpi']*4.0)
     ax = fig.add_subplot(1,1,1)
     for i in range(len(obslist)):
-        ax.errorbar(obslist[i], intoverpeak[i],yerr=(std_intoverpeak[i]/np.sqrt(len(obslist[i]))), fmt="o", color=colors[i])
-    ax.axhline(np.nanmean(intoverpeak), color="k", alpha=0.3, linestyle="--")
+        ax.errorbar(obslist[i], intoverpeak[i],yerr=(std_intoverpeak[i]/np.sqrt(len(obslist[i]))), fmt="o", color=colors[i+3], label=chans[i])
+        ax.axhline(np.nanmean(intoverpeak[i]), color=colors[i+3], alpha=0.3, linestyle="--")
     # ax.axvline(1.15, color="k", alpha=0.3, linestyle="--")
     ax.set_ylabel(f"mean(int/peak)")
     ax.set_xlabel(f"obsid")
+    ax.legend()
     fig.suptitle(f"{drift}: Int/Peak")
     plt.savefig(f"{args.project}/{drift}/{drift}_intoverpeak.{ext}", bbox_inches='tight')
-
+    plt.close(fig)
 
     fig = plt.figure(dpi=plt.rcParams['figure.dpi']*4.0)
     ax = fig.add_subplot(1,1,1)
@@ -205,14 +229,35 @@ def plt_io_pernight(
         for j in range(len(chan_obslist)):
             chanobs_intoverpeak = np.nanmean(chan_intoverpeak[j])
             chanobs_shape = np.nanmean(chan_shape[j])
-            ax.scatter(chanobs_intoverpeak,chanobs_shape, fmt="o", color=colors[i])
-    ax.axhline(1, color="k", alpha=0.3, linestyle="--")
-    ax.axvline(1, color="k", alpha=0.3, linestyle="--")
-    ax.set_ylabel(f"mean(a/b)")
+            ax.scatter(chanobs_intoverpeak,chanobs_shape, s=50, color=colors[i+3])
+    # ax.axhline(1, color="k", alpha=0.3, linestyle="--")
+    # ax.axvline(1, color="k", alpha=0.3, linestyle="--")
+    ax.set_ylabel(f"std(int/peak)")
+    ax.set_xlabel(f"mean(int/peak)")
+    fig.suptitle(f"{drift}: Int/Peak vs std")
+    ax.legend()
+    plt.savefig(f"{args.project}/{drift}/{drift}_intoverpeak_shape.{ext}", bbox_inches='tight')
+    plt.close(fig)
+
+    fig = plt.figure(dpi=plt.rcParams['figure.dpi']*4.0)
+    ax = fig.add_subplot(1,1,1)
+    for i in range(len(obslist)):
+        chan_intoverpeak = intoverpeak[i]
+        chan_std_intoverpeak = std_intoverpeak[i]
+        chan_obslist = obslist[i]
+        for j in range(len(chan_obslist)):
+            chanobs_intoverpeak = np.nanmean(chan_intoverpeak[j])
+            chanobs_std_intoverpeak = np.nanmean(chan_std_intoverpeak[j])
+            ax.scatter(chanobs_intoverpeak,chanobs_std_intoverpeak, s=50, color=colors[i+3])
+    # ax.axhline(1, color="k", alpha=0.3, linestyle="--")
+    # ax.axvline(1, color="k", alpha=0.3, linestyle="--")
+    ax.set_ylabel(f"mean(log(b/a))")
     ax.set_xlabel(f"mean(int/peak)")
     fig.suptitle(f"{drift}: Int/Peak vs shape")
-    plt.savefig(f"{args.project}/{drift}/{drift}_intoverpeak_shape.{ext}", bbox_inches='tight')
-    
+    plt.savefig(f"{args.project}/{drift}/{drift}_intoverpeak_std.{ext}", bbox_inches='tight')
+    plt.close(fig)
+
+
     return 
 
 
@@ -289,6 +334,15 @@ if __name__ == "__main__":
     refcat=  args.refcat
     logger.debug(f"{txtfile}")
 
+    ref_cat_file = f"{args.refcat}"
+    if os.path.exists(ref_cat_file):
+        do_xm = True
+    else:
+        logger.warning(f"Can't find reference GGSM catalogue for xm! ")
+        do_xm = False
+
+    
+
     # Reading in the list of obsids: will deal with the cenchan or all based on what the input txt file is called 
     if "cenchan" in txtfile:
         logger.debug(f"Only detected one cenchan, proceeding with just 1")
@@ -326,7 +380,7 @@ if __name__ == "__main__":
         cenchan_obsids = read_obsids(obs_txtfile[i])
         cenchan_good_obsids, cenchan_missing_obsids = remove_missing(cenchan_obsids)
         obsids_perchan.append(cenchan_good_obsids)
-        logger.debug(f"{len(obsids_perchan)}")
+        logger.debug(f"Number of channels included: {len(obsids_perchan)}")
 
         if args.save_missing_obsids is not None: 
             logger.debug(f"Saving missing obsids")
@@ -339,7 +393,7 @@ if __name__ == "__main__":
         obsids_postrms = []
         obsids_badrms = []
         for i in range(len(chans)):
-            logger.debug(f"{obsids_perchan[i]}")
+            logger.debug(f"Number of obsids per in channel {chans[i]}: {len(obsids_perchan[i])}")
             obsids_postrms_temp, obsids_badrms_temp = cut_high_rms(obsids_perchan[i])
             obsids_postrms.append(obsids_postrms_temp)
             obsids_badrms.append(obsids_badrms_temp)
@@ -360,14 +414,14 @@ if __name__ == "__main__":
         drift_meanshape = []
         drift_stdshape = []
         for i in range(len(chans)):
-            logger.debug(f"Running the io check")
+            logger.debug(f"Running the io check on chan {chans[i]}")
             obslist_iochecks = obsids_postrms[i]
             obs_meanintoverpeak = []
             obs_stdintoverpeak = []
             obs_meanshape = []
             obs_stdshape = []
             for o in range(len(obslist_iochecks)):
-                cat_xm, obs_intoverpeak_temp, obs_shape_temp = check_io(obslist_iochecks[o])
+                cat_xm, obs_intoverpeak_temp, obs_shape_temp = check_io(obslist_iochecks[o], do_xm)
                 obs_meanintoverpeak.append(obs_intoverpeak_temp[0])
                 obs_stdintoverpeak.append(obs_intoverpeak_temp[1])
                 obs_meanshape.append(obs_shape_temp[0])
@@ -377,11 +431,4 @@ if __name__ == "__main__":
             drift_meanshape.append(obs_meanshape)
             drift_stdshape.append(obs_stdshape)
         if args.plot in ["all", "min"]:
-            plt_io_pernight(obsids_postrms, drift_meanintoverpeak, drift_stdintoverpeak, drift_meanshape,drift)
-
-
-            
-
-
-
-
+            plt_io_pernight(obsids_postrms, drift_meanintoverpeak, drift_stdintoverpeak, drift_meanshape,drift,chans)
