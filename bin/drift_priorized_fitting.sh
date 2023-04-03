5692c30050c5c9132e43763645e63d979ab24611
astrokatross <kathryn.ross@icrar.org>
2023-04-03 15:20:51 +0800

    🐛 fix array job bug

--- bin/drift_priorized_fitting.sh
+++ bin/drift_priorized_fitting.sh
@@ -80,7 +80,7 @@ echo '#!/bin/bash' > "${script}.sbatch"
 echo "srun --cpus-per-task=${GXNCPUS} --ntasks=1 --ntasks-per-node=1 singularity run ${GXCONTAINER} ${script}" >> "${script}.sbatch"
 
 # Automatically runs a job array for each sub-band
-sub="sbatch  --begin=now+5minutes --array=1-25  --export=ALL  --time=10:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --output=${output} --error=${error}"
+sub="sbatch  --begin=now+5minutes --array=0-25  --export=ALL  --time=10:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --output=${output} --error=${error}"
 sub="${sub} ${GXNCPULINE} ${account} ${GXTASKLINE} ${depend} ${queue} ${script}.sbatch"
 if [[ ! -z ${tst} ]]
 then
