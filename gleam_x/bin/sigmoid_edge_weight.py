fcfbf75e8124db802eb1c80456e7afbf723351b8
astrokatross <kathryn.ross@icrar.org>
2023-09-20 13:33:43 +0800

    ⚡️ add mask options for weighting

--- gleam_x/bin/sigmoid_edge_weight.py
+++ gleam_x/bin/sigmoid_edge_weight.py
@@ -125,29 +125,32 @@ def create_sigweight(infits):
     print(dd.shape, dd.dtype)
 
 
+
+
     ddd=1-dd
 
     return dd, ddd
 
-def create_weightmap(sigmoidweight,rms):
+def create_weightmap(sigmoidweight,rms, mask= False):
 
     rms_fits = fits.open(rms)
     valid_mask = np.isfinite(rms_fits[0].data)
     imshape_zeros = np.zeros(valid_mask.shape)
     imshape_zeros[valid_mask] = 1.
 
-    dist_to_edge = ndimage.distance_transform_edt(imshape_zeros,sampling=[1000,1000])
-    edgemask = dist_to_edge <= np.nanmax(dist_to_edge)/5
+    if mask is True: 
+        dist_to_edge = ndimage.distance_transform_edt(imshape_zeros,sampling=[1000,1000])
+        edgemask = dist_to_edge <= np.nanmax(dist_to_edge)/5
 
 
-    lowercut = np.nanquantile(rms_fits[0].data, 0.45)
-    uppercut = np.nanquantile(rms_fits[0].data,0.95)
-    rms_mask = rms_fits[0].data <= lowercut
-    rms_mask2 = rms_fits[0].data >= uppercut
+        lowercut = np.nanquantile(rms_fits[0].data, 0.45)
+        uppercut = np.nanquantile(rms_fits[0].data,0.95)
+        rms_mask = rms_fits[0].data <= lowercut
+        rms_mask2 = rms_fits[0].data >= uppercut
 
-    combined_rms = np.logical_or(rms_mask,rms_mask2)
-    combined_mask = np.logical_and(combined_rms,edgemask)
-    rms_fits[0].data[combined_mask] = np.nan 
+        combined_rms = np.logical_or(rms_mask,rms_mask2)
+        combined_mask = np.logical_and(combined_rms,edgemask)
+        rms_fits[0].data[combined_mask] = np.nan 
     
     
     weightmap = sigmoidweight * (1/(rms_fits[0].data**2))
@@ -174,7 +177,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     dd, ddd = create_sigweight(args.infits)
 
-    # fits.writeto(args.outfits,ddd,overwrite=True)
+    fits.writeto(args.outfits,ddd,overwrite=True)
     
     weightmap = create_weightmap(ddd, args.rmsfits)
     fits.writeto(args.outfits, weightmap, overwrite=True)
