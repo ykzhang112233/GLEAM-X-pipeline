46d6b0b51af147126cdce4a7433f6d98e5528087
astrokatross <kathryn.ross@icrar.org>
2023-12-15 15:11:51 +0800

    ✨ add galactic plane check

--- gleam_x/db/check_src_fov.py
+++ gleam_x/db/check_src_fov.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
                       help="RA and Dec to check distance to source, format=x,y in deg",nargs=2)
     parser.add_argument('-s','--source',dest="source", default=None,
                       help="Source to measure")
+    parser.add_argument('-g', '--galcheck', dest="galcheck", default=False, 
+                        action="store_true", help="Do you want to check if it's a galactic pointing?")
     args = parser.parse_args()
 
     sources = {'Crab': '05:34:31.94 +22:00:52.2', 'PicA': '05:19:49.7229 -45:46:43.853', 'HydA': '09:18:05.651 -12:05:43.99', 'HerA': '16:51:11.4 +04:59:20' , 'VirA': '12:30:49.42338 +12:23:28.0439' , 'CygA': '19:59:28.35663 +40:44:02.0970' ,'CasA': '23:23:24.000 +58:48:54.00', 'CenA': '13:25:28.0 -43:01:09.00'}
@@ -51,41 +53,47 @@ if __name__ == "__main__":
         except:
             print("Cannot understand coords?")
             sys.exit(1)        
-
-    if args.source is None: 
-        # print("No source provided! going to go through all and see if any are in the fov")
-        checks = []
-        for src, coords in sources.items():
-            pos = SkyCoord([sources[src]], unit=(u.hourangle, u.deg))
-            if args.position is None: 
-                checks.append(check_coords(w,pos))
-            elif args.file is None: 
-                sep=w.separation(pos)
-                if float(sep.deg) <= 30:
-                    checks.append(True)
-                else: 
-                    checks.append(False)
-        if any(checks):
-            print("True")
-        else:
-            print("False")
-    elif args.source in sources:
-        pos = SkyCoord([sources[args.source]], unit=(u.hourangle, u.deg))
-        if args.position is None:
-            checks = check_coords(w,pos)
-        elif args.file is None: 
-            sep=w.separation(pos)
-            if float(sep.deg) <= 30:
-                checks = True
-            else: 
-                checks = False
-        if checks:
+    if args.galcheck is True: 
+        galcoords = w.galactic 
+        if abs(galcoords.b.value) <= 10.: 
             print("True")
         else: 
             print("False")
     else:
-        print(args.source+" not found in dictionary, which only contains: ", sources.keys())
-        sys.exit(1)
+        if args.source is None: 
+            # print("No source provided! going to go through all and see if any are in the fov")
+            checks = []
+            for src, coords in sources.items():
+                pos = SkyCoord([sources[src]], unit=(u.hourangle, u.deg))
+                if args.position is None: 
+                    checks.append(check_coords(w,pos))
+                elif args.file is None: 
+                    sep=w.separation(pos)
+                    if float(sep.deg) <= 30:
+                        checks.append(True)
+                    else: 
+                        checks.append(False)
+            if any(checks):
+                print("True")
+            else:
+                print("False")
+        elif args.source in sources:
+            pos = SkyCoord([sources[args.source]], unit=(u.hourangle, u.deg))
+            if args.position is None:
+                checks = check_coords(w,pos)
+            elif args.file is None: 
+                sep=w.separation(pos)
+                if float(sep.deg) <= 30:
+                    checks = True
+                else: 
+                    checks = False
+            if checks:
+                print("True")
+            else: 
+                print("False")
+        else:
+            print(args.source+" not found in dictionary, which only contains: ", sources.keys())
+            sys.exit(1)
 
 
 
